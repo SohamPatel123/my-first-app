89dfdf5afa22f6077c2de1a9227c54e83f777861
SohamPatel123 <105942884+SohamPatel123@users.noreply.github.com>
2022-06-01 00:05:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { React} from "react";
+import { CirclePicker } from "react-color";
+import Grid from "./Grid";
+
+function App() {
+
+window.onbeforeunload = function(){
+  sessionStorage.setItem("origin", window.location.href);
+  sessionStorage.setItem("color","white");
+  localStorage.setItem("Grid",JSON.stringify(Grid));
+}
+window.onload = function(){
+  if(window.location.href === sessionStorage.getItem("origin")){
+    sessionStorage.clear();
+  }
+}
+const handleChangeComplete = (color) => {
+  console.log(color.hex);
+  sessionStorage.setItem("color",color.hex);
+};
+
+return (<>
+  <div className="App">
+  <CirclePicker className="circles"onChangeComplete={handleChangeComplete} />
+  </div>
+  </>);
+}
+export default App;
